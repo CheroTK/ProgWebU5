87ee2b8c73063339e82dc92aec8d1db46cc7f870
CheroTK <a22490076@itmexicali.edu.mx>
2025-05-29 23:51:51 -0700

    config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 class Config:
-    SECRET_KEY = '1234'
-    MYSQL_HOST = 'localhost'
+    SECRET_KEY = '13579'
+    MYSQL_HOST = 'trolley.proxy.rlwy.net'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'constanze135'
-    MYSQL_DB = 'activelife_db'
+    MYSQL_PASSWORD = 'ULGdXOhhXWhHVVySaRQIjokUNDOUYaNG'
+    MYSQL_DB = 'railway'
+    MYSQL_PORT = '59930'
